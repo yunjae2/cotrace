84eba8ea2d291d2bc57376ea42b38dec1e3a6cc7
Yunjae Lee <lyj7694@gmail.com>
2019-05-01 05:59:16 +0900

    trace.h: Use buffer instead of instant printing
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- trace.h
+++ trace.h
@@ -17,6 +17,7 @@
 #define RELTIME(time)		(time - start_time)
 
 #define MAX_TRACE_DEPTH		1000
+#define MAX_BUF_OFFSET		1000
 #define TRACE_PACK(pkg, _ctx, _time)	{\
 	pkg.ctx = _ctx;\
 	pkg.time = _time;\
@@ -27,6 +28,11 @@
 }
 #define TRACE_PUSH(data)	(trace_stack[trace_depth++] = data)
 #define TRACE_POP()		(trace_stack[--trace_depth])
+#define TRACE_WRITE(data)	{\
+	trace_buf[buf_offset++] = data;\
+	if (buf_offset >= MAX_BUF_OFFSET)\
+		flush_trace_buf();\
+}
 
 
 struct trace_data {
@@ -34,6 +40,7 @@ struct trace_data {
 	unsigned long time;
 };
 
+void flush_trace_buf(void) __attribute__ ((no_instrument_function));
 void trace_begin(void) __attribute__ ((constructor, no_instrument_function));
 void trace_end(void) __attribute__ ((destructor, no_instrument_function));
 void __cyg_profile_func_enter(void *this_fn, void *call_site)
@@ -48,6 +55,16 @@ struct trace_data trace_stack[MAX_TRACE_DEPTH];
 unsigned long start_time;
 static FILE *fp_trace;
 
+int buf_offset;
+struct trace_data trace_buf[MAX_BUF_OFFSET];
+
+
+void flush_trace_buf(void)
+{
+	fwrite(trace_buf, sizeof(struct trace_data) * buf_offset, 1, fp_trace);
+	buf_offset = 0;
+}
+
 void trace_begin(void)
 {
 	struct timespec ts;
@@ -55,16 +72,20 @@ void trace_begin(void)
 
 	nr_ctx = 0;
 	trace_depth = 0;
+	buf_offset = 0;
 
 	fp_trace = fopen("trace.out", "w");
 
 	GETRES(res, ts);
-	fprintf(fp_trace, "# Time res: %lu ns\n", res);
 	GETTIME(start_time, ts);
+	printf("struct size: %ld\n", sizeof(struct trace_data));
 }
 
 void trace_end(void)
 {
+	if (buf_offset)
+		flush_trace_buf();
+
 	if (fp_trace)
 		fclose(fp_trace);
 }
@@ -77,10 +98,9 @@ void __cyg_profile_func_enter(void *this_fn, void *call_site)
 
 	curr_ctx = nr_ctx++;
 	GETTIME(time, ts);
-	fprintf(fp_trace, "ctx start: %d\t\t time: %lu\n",
-			curr_ctx, RELTIME(time));
 
-	TRACE_PACK(tdata, curr_ctx, time);
+	TRACE_PACK(tdata, curr_ctx, RELTIME(time));
+	TRACE_WRITE(tdata);
 	TRACE_PUSH(tdata);
 }
 
@@ -95,8 +115,8 @@ void __cyg_profile_func_exit(void *this_fn, void *call_site)
 	TRACE_UNPACK(tdata, curr_ctx, ctx_start_time);
 
 	GETTIME(ctx_end_time, ts);
-	fprintf(fp_trace, "ctx end: %d\t\t time: %lu\n",
-			curr_ctx, RELTIME(ctx_end_time));
+	TRACE_PACK(tdata, curr_ctx, RELTIME(ctx_end_time));
+	TRACE_WRITE(tdata);
 }
 
 #endif
